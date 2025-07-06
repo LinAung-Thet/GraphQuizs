68f8bec04f073f3be61538ca6b13cffd7eb72c21
LinAung-Thet <laungthet@gmail.com>
2025-07-06 23:24:57 +1200

    Refactored for optimization.

--- MaxProbability.cpp
+++ MaxProbability.cpp
@@ -6,40 +6,37 @@ using namespace std;
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        //Build the graph which contains pairs of (dest, probability)
-        vector<vector<pair<int,double>>> graph(n);
-        for(int i=0; i<edges.size(); i++) {
-            auto& e = edges[i];
-            graph[e[0]].push_back(pair(e[1], succProb[i]));
-            graph[e[1]].push_back(pair(e[0], succProb[i]));
+        vector<vector<pair<int, double>>> graph(n);
+        for (int i = 0; i < edges.size(); ++i) {
+            int u = edges[i][0], v = edges[i][1];
+            double prob = succProb[i];
+            graph[u].emplace_back(v, prob);
+            graph[v].emplace_back(u, prob);
         }
 
-        //Vector which stores the max probability to each node from the source
-        vector<double> results(n, 0);
+        vector<double> probTo(n, 0.0);
+        probTo[start_node] = 1.0;
 
-        //Queue which stores the potential nodes to the destination
-        queue<pair<int, double>> q;
-        
-        //Start from start_node
-        q.emplace(pair(start_node, 1));
+        // Max heap: (probability, node)
+        priority_queue<pair<double, int>> pq;
+        pq.emplace(1.0, start_node);
 
-        //Repeat until there is no more potential node found
-        while(!q.empty()) {
-            auto [cN, cP] = q.front(); q.pop();
+        while (!pq.empty()) {
+            auto [currProb, node] = pq.top();
+            pq.pop();
 
-            //Traverse every node the current node is directly connected
-            for(auto [d,p]: graph[cN]) {
-                //If it has the bigger probability, proceed it
-                double nP = cP * p;
-                if(nP > results[d]) {
-                    q.emplace(pair(d, nP));
-                    //Update the results
-                    results[d] = nP;
+            if (node == end_node) return currProb;
+
+            for (auto& [neighbor, edgeProb] : graph[node]) {
+                double newProb = currProb * edgeProb;
+                if (newProb > probTo[neighbor]) {
+                    probTo[neighbor] = newProb;
+                    pq.emplace(newProb, neighbor);
                 }
             }
         }
 
-        return results[end_node];
+        return 0.0;
     }
 };
 //Test cases
