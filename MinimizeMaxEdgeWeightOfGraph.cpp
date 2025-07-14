a4d3c0c51af3a52694f259f9056d207170ea3f3b
LinAung-Thet <laungthet@gmail.com>
2025-07-14 12:18:06 +1200

    Modified the program to cater the test case 5.

--- MinimizeMaxEdgeWeightOfGraph.cpp
+++ MinimizeMaxEdgeWeightOfGraph.cpp
@@ -10,66 +10,32 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumMaxWeight(int n, int threshold, vector<vector<int>>& edges) {
-        int left = 0, right = 0;
-        for (auto& e : edges) right = max(right, e[2]);
-
-        int answer = -1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isValid(n, threshold, edges, mid)) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return answer;
+    int dfs(int i, int m, vector<vector<pair<int, int>>> &ral, vector<bool> &vis) {
+        int res = 1; vis[i] = true;
+        for (auto [j, w] : ral[i])
+            if (w <= m && !vis[j])
+                res += dfs(j, m, ral, vis);
+        return res;
     }
-
-private:
-    bool isValid(int n, int threshold, vector<vector<int>>& edges, int maxWeight) {
-        vector<vector<int>> graph(n);       // forward graph
-        vector<vector<int>> reverseGraph(n); // for reachability to node 0
-        vector<int> outdegree(n, 0);
-
-        for (auto& e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            if (w <= maxWeight) {
-                graph[u].push_back(v);
-                reverseGraph[v].push_back(u);
-                outdegree[u]++;
-            }
+    int minimumMaxWeight(int n, int threshold, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> ral(n);
+        for (auto &e : edges)
+            ral[e[1]].push_back({e[0], e[2]});
+        
+        int l = 1, r = 1000001;
+        
+        while (l < r) {
+            int m = (l + r) / 2;
+            
+            vector<bool> vis(n, false);
+            
+            if (dfs(0, m, ral, vis) == n)
+                r = m;
+            else
+                l = m + 1;
         }
-
-        // Check outdegree constraint
-        for (int i = 0; i < n; ++i) {
-            if (outdegree[i] > threshold) return false;
-        }
-
-        // Check if all nodes can reach node 0 (i.e., node 0 is reachable from all nodes)
-        vector<bool> visited(n, false);
-        queue<int> q;
-        q.push(0);
-        visited[0] = true;
-
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            for (int v : reverseGraph[u]) {
-                if (!visited[v]) {
-                    visited[v] = true;
-                    q.push(v);
-                }
-            }
-        }
-
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i]) return false;
-        }
-
-        return true;
+        
+        return l == 1000001 ? -1 : l;
     }
 };
 
@@ -111,7 +77,7 @@ int main() {
     cout << "Minimum max edge weight: " << result << ", Expected: -1" << endl; 
 
     // Test case 5
-    edges = {{2,0,39},{2,1,72},{2,3,67},{1,2,78},{3,0,10},{0,2,81}};
+    edges = {{2,0,39},{2,1,72},{2,3,67},{1,2,78},{3,0,10},{0,2,81},{1,0,78},{1,3,78}};
     n = 4;
     outDegreeThreshold = 2;
     result = solution.minimumMaxWeight(n,outDegreeThreshold, edges);
